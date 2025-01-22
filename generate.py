8db3ebb7d6ed35eab0f209b86071f655b965be12
skylerheininger <skyler.heininger@uvm.edu>
2025-01-22 11:44:32 -0500

    Final version of assignment 2

--- generate.py
+++ generate.py
@@ -1,8 +1,31 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
+# pyrosim.Send_Cube(name="Box", pos=[0, 0, .5], size=[1, 2, 3])
+# pyrosim.Send_Cube(name="Box", pos=[0, 0, .5], size=[1, 1, 1])
+# pyrosim.Send_Cube(name="Box2", pos=[1, 0, 1.5], size=[1, 1, 1])
+
+# Variables for boxes
+length, width, height = 1, 1, 1
+
+x = 0
+while x < 5:
+    y = 0
+    while y < 5:
+        # Use total height to find height of next box
+        total_height = 0
+        i = 0
+        length, width, height = 1, 1, 1
+        while i < 10:
+            pyrosim.Send_Cube(name=f"Box{i}{x}{y}", pos=[x, y, total_height + height / 2], size=[length, width, height])
+            length *= 0.9
+            width *= 0.9
+            total_height += height
+            height *= 0.9
+            i += 1
+        y += 1
+    x += 1
 
 pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -1,13 +1,20 @@
 import pybullet as p
 import time
+import pybullet_data
+
 
 physicsClient = p.connect(p.GUI)
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
-p.loadSDF("box.sdf")
+p.setGravity(0, 0, -9.8)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+planeId = p.loadURDF("plane.urdf")
+
+
+p.loadSDF("boxes.sdf")
 
 for i in range(0, 1000):
     p.stepSimulation()
-    time.sleep(1/60)
+    time.sleep(1 / 60)
     print(i)
 
 p.disconnect()
