6bb6a07f0e7d86f4b9ed839b2ffbf18214f6ed18
skylerheininger <skyler.heininger@uvm.edu>
2025-02-10 10:38:06 -0500

    step 40

--- analyze.py
+++ analyze.py
@@ -1,11 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
-data_back = np.load("data/backLegSensor.npy")
-data_front = np.load("data/frontLegSensor.npy")
+# data_back = np.load("data/backLegSensor.npy")
+# data_front = np.load("data/frontLegSensor.npy")
+#
+#
+# plt.plot(data_back, linewidth=3, label="Back Leg")
+# plt.plot(data_front, label="Front Leg")
+# plt.legend()
+# plt.show()
 
 
-plt.plot(data_back, linewidth=3, label="Back Leg")
-plt.plot(data_front, label="Front Leg")
+data_angles = np.load("data/manualAngles.npy")
+
+import matplotlib.pylab as plt
+x = np.linspace(-np.pi, np.pi, 1000)
+plt.plot(x, data_angles, linewidth=3, label="Front leg motor values")
+plt.plot(x, data_angles, linewidth=0.5, label="Back leg motor values")
 plt.legend()
 plt.show()
+
--- simulate.py
+++ simulate.py
@@ -3,6 +3,8 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
+import random
+import math
 
 
 physicsClient = p.connect(p.GUI)
@@ -16,11 +18,25 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(1000)
-frontLegSensorValues = numpy.zeros(1000)
+iterations = 1000
+
+backLegSensorValues = numpy.zeros(iterations)
+frontLegSensorValues = numpy.zeros(iterations)
+target_angles = numpy.zeros(iterations)
+
+angles = numpy.linspace(0, 2 * math.pi, iterations)
+
+# target_angles = math.pi / 4 * numpy.sin(angles)
 
 
-for i in range(0, 1000):
+amplitude = math.pi / 4
+frequency = 10 / (iterations / (2 * math.pi))
+phaseOffset = 0
+
+for i in range(0, iterations):
+    target_angle = amplitude * numpy.sin(frequency * i + phaseOffset)
+    target_angles[i] = target_angle
+
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
@@ -32,9 +48,10 @@ for i in range(0, 1000):
 
         controlMode=p.POSITION_CONTROL,
 
-        targetPosition=-numpy.pi/4,
+        # targetPosition=-math.pi / 4 + math.pi / 2 * random.random(),
+        targetPosition=target_angles[i],
 
-        maxForce=500)
+        maxForce=40)
 
     pyrosim.Set_Motor_For_Joint(
 
@@ -44,9 +61,10 @@ for i in range(0, 1000):
 
         controlMode=p.POSITION_CONTROL,
 
-        targetPosition=numpy.pi / 4,
+        # targetPosition=-math.pi / 4 + math.pi / 2 * random.random(),
+        targetPosition=target_angles[i],
 
-        maxForce=500)
+        maxForce=40)
     # print(backLegTouch)
     time.sleep(1 / 60)
     # print(i)
@@ -54,6 +72,7 @@ for i in range(0, 1000):
 p.disconnect()
 
 print(backLegSensorValues)
-numpy.save("data/backLegSensor.npy", backLegSensorValues)
-numpy.save("data/frontLegSensor.npy", frontLegSensorValues)
+# numpy.save("data/backLegSensor.npy", backLegSensorValues)
+# numpy.save("data/frontLegSensor.npy", frontLegSensorValues)
+# numpy.save("data/manualAngles.npy", targetAngles)
 
